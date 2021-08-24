3a2c16dba0a4f07921b8a620aadb012d5f137b42
Bernard Belanger <bbelanger@emoneyadvisor.com>
2021-08-24 17:22:33 -0400

    init

---
+++ Marvel/Character.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Marvel
+{
+    public class Character
+    {
+        public string Name { get; set; }
+
+        public static List<Character> FindByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Marvel/Comic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Marvel
+{
+    public class Comic
+    {
+        public string Title { get; set; }
+        public DateTime SaleDate { get; set; }
+
+        /// <summary>
+        /// Limited to the 1 comic ordered by Name Ascending
+        /// </summary>
+        /// <param name="title"></param>
+        /// <returns></returns>
+        public static Comic GetComic(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Limited to the top 5 comics ordered by Name Ascending
+        /// </summary>
+        /// <param name="character"></param>
+        /// <returns></returns>
+        public static List<Comic> GetComics(Character character)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
